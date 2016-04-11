8c46f7b9021990045fbb91ef581abc029c69634e
priteshparekh77 <ratedr77@gmail.com>
2016-04-10 22:05:45 -0400

    Update database.php

--- OnlineShopping/database.php
+++ OnlineShopping/database.php
@@ -1,6 +1,6 @@
 <?php 
  // Connects to Our Database  
- 	$con=mysqli_connect("localhost","parekhp","sundarkand","parekhp_OnlineShopping");
+ 	$con=mysqli_connect("localhost","parekhp","","parekhp_OnlineShopping");
 	if (!$con)
 	  {
 	  	die('Could not connect: ' . mysqli_error());
